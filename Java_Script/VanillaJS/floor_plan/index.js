b238a24f3933ed5572e3e529e684ec473fd446cb
xilucks <siun0331@gmail.com>
2021-08-04 23:38:56 +0900

    아파트 도면도 / 마우스 위치에 따른 방 이름 출력

---
+++ Java_Script/VanillaJS/floor_plan/index.js
@@ -0,0 +1,18 @@
+function showTitle(e) {
+	if (e.target.dataset.title) {
+	  const span = document.createElement('span');
+	  span.classList.add('title');
+	  span.textContent = e.target.dataset.title;
+	  e.target.append(span);
+	}
+  }
+  
+  function removeTitle(e) {
+	if (e.target.dataset.title) {
+	  e.target.lastElementChild.remove();
+	}
+  }
+  
+  const map = document.querySelector('.map');
+  map.addEventListener('mouseover', showTitle);
+  map.addEventListener('mouseout', removeTitle);
