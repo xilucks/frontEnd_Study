80c539546c7648e63215c595cad62d4d381c1c29
xilucks <siun0331@gmail.com>
2021-09-30 12:45:49 +0900

    ES6 JS 상속 클래스

--- Java_Script/VanillaJS/TILCode/Es6Class.js
+++ Java_Script/VanillaJS/TILCode/Es6Class.js
@@ -16,3 +16,18 @@ class Shape{
 
 var s =new Shape(0,0);
 s.area();
+
+
+class Circle extends Shape {
+    constructor(x, y, radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+    area() {
+        if (this.radius === 0 ) return super.area();
+        return this.radius * this.radius;
+    }
+}
+
+var c = new Circle (0,0, 10);
+c.area();
